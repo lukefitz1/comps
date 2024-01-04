bbe2d45513fc83f0e1e8ed71362c202edd47a5c0
Lucas Fitzgerald <lucas.fitzgerald@liveoak.bank>
2024-01-04 15:54:23 -0500

    starting on reducer

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,30 +1,10 @@
-import { useState } from "react";
-import Table from "./Table";
 import { GoArrowDown, GoArrowUp } from "react-icons/go";
+import Table from "./Table";
+import useSort from "../hooks/use-sort";
 
 function SortableTable(props) {
-  const [sortOrder, setSortOrder] = useState(null);
-  const [sortBy, setSortBy] = useState(null);
   const { config, data } = props;
-
-  const handleClick = (label) => {
-    if (sortBy && label !== sortBy) {
-      setSortOrder("asc");
-      setSortBy(label);
-      return;
-    }
-
-    if (sortOrder === null) {
-      setSortOrder("asc");
-      setSortBy(label);
-    } else if (sortOrder === "asc") {
-      setSortOrder("desc");
-      setSortBy(label);
-    } else if (sortOrder === "desc") {
-      setSortOrder(null);
-      setSortBy(null);
-    }
-  };
+  const { handleClick, sortBy, sortOrder, sortedData } = useSort(config, data);
 
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) return column;
@@ -44,23 +24,6 @@ function SortableTable(props) {
       ),
     };
   });
-
-  let sortedData = data;
-  if (sortOrder && sortBy) {
-    const { sortValue } = config.find((column) => column.label === sortBy);
-    sortedData = [...data].sort((a, b) => {
-      const valueA = sortValue(a);
-      const valueB = sortValue(b);
-
-      const reverseOrder = sortOrder === "asc" ? 1 : -1;
-
-      if (typeof valueA === "string") {
-        return valueA.localeCompare(valueB) * reverseOrder;
-      } else {
-        return (valueA - valueB) * reverseOrder;
-      }
-    });
-  }
 
   return <Table {...props} data={sortedData} config={updatedConfig} />;
 }
